606960c7bcdc8f55772c62995fbce3d6733af125
Ken Jannette <KenJannette@kens-mbp.attlocal.net>
2018-07-05 12:41:04 -0400

    Edited accounts/views.py to redirect home after new account created

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ def signup(request):
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 login(request, user)
-                return render(request, 'accounts/signup.html')
+                return redirect('home')
         else:
             return render(request, 'accounts/signup.html', {'error': 'Passwords did not match'})
 
