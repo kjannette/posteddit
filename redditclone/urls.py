35f9b26f424ca6ce8e5577e54861f87d051da008
Kenneth Jannette <kennethjannette@Kenneths-Air.router>
2018-07-20 12:44:41 -0700

    latest commit

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf.urls import include
 from django.urls import include, path
 from django.conf.urls import url
 import accounts.views
