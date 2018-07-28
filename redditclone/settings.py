27b83dc0c8b22a4d8e69f4eecf804876448f2c63
Kenneth Jannette <kennethjannette@Kenneths-Air.localdomain>
2018-07-28 09:04:54 -0700

    fixing settings

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
