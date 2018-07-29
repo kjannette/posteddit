898b7a475caad04af38b731374a603e157c2b3cf
Ken Jannette <ken.jannette@gmail.com>
2018-07-29 11:40:27 -0700

    heroku

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +132,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
